30502d7306de57b0e41bb2f90a2a704e8d698c4e
Steph Parker <stephparker1994@gmail.com>
2018-10-28 16:03:43 +0000

    fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,18 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(3)).to eq 'fizz'  
+  end
+  
+  it 'returns "fizz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz' 
+  end
+  
+  it 'returns "fizz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz' 
+  end
+  
+  it 'returns "fizz" when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
   end
 end
